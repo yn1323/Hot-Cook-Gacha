d1da189b5ac11168917d9da6569c4c4407c239fd
y-natani <yn1323@gmail.com>
2023-07-30 22:26:09 +0900

    fix: story

--- app/_components/layout/Center/index.stories.tsx
+++ app/_components/layout/Center/index.stories.tsx
@@ -1,14 +1,16 @@
-import { type Meta, type StoryObj } from "@storybook/react";
+import { type Meta, type StoryObj } from '@storybook/react'
 import { FormProviderDecorator } from '@/config/Decorators'
-import { Center  } from ".";
+import { Center } from '.'
 
 const meta = {
   title: 'layout/Center',
-  component: Center ,
-  args: {},
+  component: Center,
+  args: {
+    children: <div>aaa</div>,
+  },
   parameters: {},
   decorators: [FormProviderDecorator],
-} satisfies Meta<typeof Center >;
-export default meta;
+} satisfies Meta<typeof Center>
+export default meta
 
-export const Basic: StoryObj<typeof meta> = {};
+export const Basic: StoryObj<typeof meta> = {}
