270087f05874746f78b56eb5417c1effb2f9abcf
y-natani <yn1323@gmail.com>
2023-08-16 00:31:21 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const withPWA = require('next-pwa')
 /** @type {import('next').NextConfig} */
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
+
 module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-  },
   experimental: {
     serverActions: true,
   },
